5afa8106a7f33cadf4d2510a8efce85c0c6d876b
zhaiyanwen <15815361527@qq.com>
2018-01-02 17:15:46 +0800

    https://github.com/zhaiyanwen/yan.git

---
+++ src/ken/jiha.java
@@ -0,0 +1,22 @@
+package ken;
+
+public class jiha {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("yujianni");
+int a;
+int b;
+
+int result =add(5,10);
+System.out.println(result);
+
+
+	}
+public static int add(int c,int d){
+	return c+d;
+	
+	
+	
+}
+}
